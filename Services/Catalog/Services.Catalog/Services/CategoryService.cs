b9e845f899cce3e3e3234c86b3d2161f60fc2b17
Parviz <parviz.hajili@gmail.com>
2022-09-08 12:09:41 +0400

    add ICategoryservice and CategoryService

---
+++ Services/Catalog/Services.Catalog/Services/CategoryService.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using MongoDB.Driver;
+using Services.Catalog.Dtos;
+using Services.Catalog.Models;
+using Services.Catalog.Settings;
+using Shared.Dtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Services.Catalog.Services
+{
+    internal class CategoryService: ICategoryService
+    {
+        private readonly IMongoCollection<Category> _categoryCollection;
+        private readonly IMapper _mapper;
+
+        public CategoryService(IMapper mapper,DatabaseSettings databaseSettings)
+        {
+            var client = new MongoClient(databaseSettings.ConnectionString);
+            var database = client.GetDatabase(databaseSettings.DatabaseName);
+
+            _categoryCollection = database.GetCollection<Category>(databaseSettings.CourseCollectionName);
+            _mapper = mapper;
+        }
+
+        public async Task<Response<List<CategoryDto>>> GetAllAsync()
+        {
+            var categories = await _categoryCollection.Find(category=>true).ToListAsync();
+
+            return Response<List<CategoryDto>>.Success(_mapper.Map<List<CategoryDto>>(categories), 200);
+        }
+
+        public async Task<Response<CategoryDto>> CreateAsync(Category category)
+        {
+            await _categoryCollection.InsertOneAsync(category);
+
+            return Response<CategoryDto>.Success(_mapper.Map<CategoryDto>(category),200);
+        }
+
+        public async Task<Response<CategoryDto>> GetByIdAsync(string id)
+        {
+            var category = await _categoryCollection.Find(c => c.Id == id).FirstOrDefaultAsync();
+            if (category == null)
+                return Response<CategoryDto>.Fail("Category Not Found",404);
+            else
+                return Response<CategoryDto>.Success(_mapper.Map<CategoryDto>(category), 200);
+        }
+    }
+}
