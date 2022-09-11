dfeb7b5edac8a3a004b7652b1087a26fd486d30d
Parviz <parviz.hajili@gmail.com>
2022-09-11 18:04:55 +0400

    Setup IdentityServer

---
+++ IdentityServer/Microservices.Identityserver/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Microservices.Identityserver.Models
+{
+    // Add profile data for application users by adding properties to the ApplicationUser class
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
